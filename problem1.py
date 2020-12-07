c6e10ca107a9ebf703d0bba227ae98b4c73d53d3
Tate Chin <tate.chin23@ycdsbk12.ca>
2020-12-07 18:07:51 +0000

    Finished test

--- problem1.py
+++ problem1.py
@@ -1,1 +1,18 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem1.py
+Purpose:	A program that lets you enter degrees in celsius and converts in Fahrenheit 
+Author:	Fabroa.E
+Created: 0/12/2020
+------------------------------------------------------------------------------
+'''
+
+# Gets celsius from the user
+celsius = float(input("Enter a temperature in celsius, and my program will convert it to fahrenheit: "))
+
+# Stores its value in fahrenheit inside of this variable
+fahrenheit = (9/5)*celsius + 32
+
+# Prints out the variable (result)
+print("Your temperature in fahrenheit is: {} degrees".format(fahrenheit))
 
--- problem2.py
+++ problem2.py
@@ -1,1 +1,20 @@
+'''
+-------------------------------------------------------------------------------
+Name:		problem2.py
+Purpose:	A program that distrubutes chicken wings evenly among students, then sets aside the rest for teacher
+Author:	Fabroa.E
+Created: 07/12/2020
+------------------------------------------------------------------------------
+'''
 
+# Gets number of students and number of chicken wings from user
+number_of_students = int(input("Enter the number of students in Mr.Fabroa's class: "))
+number_of_chicken_wings = int(input("Enter the number of chicken wings to be distributed: "))
+
+# This part distributes the chicken wings the way described in the question
+chicken_wings_per_student = number_of_chicken_wings // number_of_students
+left_over = number_of_chicken_wings % number_of_students
+
+# prints out the result here
+print("Number of chicken wings for each student is: {}".format(chicken_wings_per_student))
+print("Number of chicken wings for Mr.Fabroa is: {}".format(left_over))
